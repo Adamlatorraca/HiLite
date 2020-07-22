187ffe2b1417972092dadc2a002c2a0f2b74e270
Adamlatorraca <adam.latorraca@gmail.com>
2020-07-22 18:10:16 -0400

    add new and create actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,15 @@
 class UsersController < ApplicationController
+
+    def new
+        @user = User.new
+    end
+
+    def create
+    end
+
+private
+
+    def user_params
+        params.require(:user).permit(:username, :password, :password_confirmation)
+    end
 end
