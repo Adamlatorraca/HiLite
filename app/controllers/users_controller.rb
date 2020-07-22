8396ee3f5b0fe4bfc65f5cc82ee3d08e8c26b84b
Adamlatorraca <adam.latorraca@gmail.com>
2020-07-22 18:14:20 -0400

    add user creation logic

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,11 +5,18 @@ class UsersController < ApplicationController
     end
 
     def create
+        @user = User.new(user_params)
+        if @user.save
+            session[:user_id] = @user.id
+            redirect_to user_path(@user)
+        else
+            render :new
+        end
     end
 
 private
 
     def user_params
-        params.require(:user).permit(:username, :password, :password_confirmation)
+        params.require(:user).permit(:username, :password)
     end
 end
