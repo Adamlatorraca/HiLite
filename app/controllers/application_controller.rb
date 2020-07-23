20a789517f966d0cc60f24944ebf34a8548a26e2
Adamlatorraca <adam.latorraca@gmail.com>
2020-07-23 12:54:04 -0400

    add helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,20 @@
 class ApplicationController < ActionController::Base
+    helper_method :current_user, :signed_in?
+
+
+private
+  def current_user
+    # if limits db queries
+    @current_user ||= User.find_by_id(session[:user_id]) if session[:user_id]
+  end
+
+  def signed_in?
+    # most effiecient
+    !!session[:user_id]
+  end
+
+  def redirect_if_not_signed_in
+    redirect_to '/' if !signed_in?
+  end
+
 end
