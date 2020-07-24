f9ec909a2f0258a7ed45d054f5df0a6502ce9040
Adamlatorraca <adam.latorraca@gmail.com>
2020-07-23 23:23:12 -0400

    add category name helper for index header

--- app/helpers/hilites_helper.rb
+++ app/helpers/hilites_helper.rb
@@ -1,2 +1,10 @@
 module HilitesHelper
+
+    def hilites_header
+        if @category
+            tag.h1("#{@category.name.capitalize} HiLites")
+        else
+            tag.h1("All HiLites")
+        end
+    end
 end
