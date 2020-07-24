cd7650ed99ccc06e3fd7812ebacfe6f914434153
Adamlatorraca <adam.latorraca@gmail.com>
2020-07-23 21:00:14 -0400

    add create action and fixed redicrection

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,18 @@
 class SessionsController < ApplicationController
-    
+
     def destroy
         session.clear
-        root_path
+        redirect_to root_path
     end
 
     def create
-        #session[:user]
+        user = User.find_by(username: params[:user][:username])
+        if user && user.authenticate(params[:user][:password])
+            session[:user_id] = user.id
+            redirect_to user_path(user)
+        else
+            flash[:message] = "Invalid Sign In info, please try again."
+            redirect_to '/signin'
+        end
     end
 end
