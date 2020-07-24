ee63d986299b415b2b724523228fd94470935b30
Adamlatorraca <adam.latorraca@gmail.com>
2020-07-23 20:59:48 -0400

    add new and create actions

--- app/controllers/hilites_controller.rb
+++ app/controllers/hilites_controller.rb
@@ -1,2 +1,22 @@
 class HilitesController < ApplicationController
+    before_action :redirect_if_not_signed_in
+
+    def new
+        @hilite = Hilite.new
+    end
+
+    def create
+        @hilite = current_user.hilites.build(hilite_params)
+        if @hilite.save
+            redirect_to hilites_path
+        else
+            render :new
+        end
+    end
+
+private
+
+    def hilite_params
+        params.require(:hilite).permit(:title, :content)
+    end
 end
