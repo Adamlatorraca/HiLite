5ea7f67c276a2fb2cc3bb58665b2156f50ddc0f0
Adamlatorraca <adam.latorraca@gmail.com>
2020-07-24 21:52:02 -0400

    add new, create actions in comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,26 @@
 class CommentsController < ApplicationController
-end
+    #skip_before_action :verify_authenticity_token
+
+    def new
+        if params[:hilite_id] && @hilite = Hilite.find_by_id(params[:hilite_id])
+            @comment = @hilite.comments.build
+        else
+            @comment = Comment.new 
+        end
+    end
+
+    def create
+        @comment = current_user.comments.build(comment_params)
+        if @comment.save
+            redirect_to hilite_path(@comment.hilite)
+        else
+            render :new
+        end
+    end
+  
+    private
+  
+    def comment_params
+      params.require(:comment).permit(:content, :hilite_id, :user_id)
+    end
+  end
