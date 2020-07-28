5ae6b04d41d0026057dfac575f7a0382494a170a
Adamlatorraca <adam.latorraca@gmail.com>
2020-07-27 20:23:37 -0400

    add uid, provider, and email attributes to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_22_212617) do
+ActiveRecord::Schema.define(version: 2020_07_27_194610) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -44,6 +44,9 @@ ActiveRecord::Schema.define(version: 2020_07_22_212617) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "uid"
+    t.string "provider"
+    t.string "email"
   end
 
 end
