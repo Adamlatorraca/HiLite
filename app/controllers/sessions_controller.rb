9c9cb350bb960a50564f8386d7da43c173dd0809
Adamlatorraca <adam.latorraca@gmail.com>
2020-07-27 20:20:48 -0400

    add omniauth custom action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,14 @@
 class SessionsController < ApplicationController
 
-    def destroy
-        session.clear
-        redirect_to root_path
+    def omniauth  #log users in with omniauth
+        user = User.create_from_omniauth(auth)
+        if user.valid?
+            session[:user_id] = user.id
+            redirect_to user
+        else
+            flash[:message] = user.errors.full_messages.join(", ")
+            redirect_to root_path
+        end
     end
 
     def create
@@ -14,5 +20,15 @@ class SessionsController < ApplicationController
             flash[:message] = "Invalid Sign In info, please try again."
             redirect_to '/signin'
         end
+    end
+
+    def destroy
+        session.clear
+        redirect_to root_path
+    end
+
+private
+    def auth
+        request.env['omniauth.auth']
     end
 end
