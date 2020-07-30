0951a9b971615acb064c8d26cb954fd852be99ec
Adamlatorraca <adam.latorraca@gmail.com>
2020-07-29 22:56:50 -0400

    add title, category, and content validations

--- app/models/hilite.rb
+++ app/models/hilite.rb
@@ -1,7 +1,15 @@
 class Hilite < ApplicationRecord
   belongs_to :user
   belongs_to :category
+
   has_many :comments
   has_many :users, through: :comments
-  accepts_nested_attributes_for :user
+
+  accepts_nested_attributes_for :user, :category
+
+  validates_presence_of :title
+  validates_presence_of :category
+  validates_presence_of :content
+  validates_uniqueness_of :content
+
 end
