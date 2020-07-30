7c7a206bf46e5e9f5e990461764c660fc15ce617
Adamlatorraca <adam.latorraca@gmail.com>
2020-07-29 22:54:26 -0400

    add new category feature

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,26 @@
 class CategoriesController < ApplicationController
+    before_action :redirect_if_not_signed_in, only: [:new, :create]
+    
     def index
         @categories = Category.all
+    end
+
+    def new
+        @category = Category.new
+    end
+
+    def create
+        @category = Category.new(category_params)
+        if @category.save
+            redirect_to categories_path
+        else
+            render :new
+        end
+    end
+
+private
+
+    def category_params
+        params.require(:category).permit(:name)
     end
 end
