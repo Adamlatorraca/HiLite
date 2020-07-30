b9bd191854f1675e43f20b39d186f8fade9e553e
Adamlatorraca <adam.latorraca@gmail.com>
2020-07-29 22:56:21 -0400

    add name validations

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,5 @@
 class Category < ApplicationRecord
     has_many :hilites
+    validates :name, presence: :true 
+    validates_uniqueness_of :name, message: "that category has already been created."
 end
