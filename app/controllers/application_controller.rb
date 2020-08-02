4363549def6d3ad2414c1919eabf2dd67375e58c
Adamlatorraca <adam.latorraca@gmail.com>
2020-08-02 17:40:50 -0400

    add error handling for missing record

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   include ApplicationHelper
+  rescue_from ActiveRecord::RecordNotFound, with: :handle_record_not_found
+
+  private
+
+  def handle_record_not_found
+    render :"shared/not_found"
+  end
 end
