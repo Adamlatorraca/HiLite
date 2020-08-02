70a7eb8b23ca81b33e1be381d807eb5c3e7b9408
Adamlatorraca <adam.latorraca@gmail.com>
2020-08-02 15:47:05 -0400

    move helper methods

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+    def current_user
+        # if limits db queries
+        @current_user ||= User.find_by_id(session[:user_id]) if session[:user_id]
+    end
+    
+    def signed_in?
+        # most effiecient
+        !!session[:user_id]
+    end
+
+    def redirect_if_not_signed_in
+        flash[:error] = "Please Sign in."
+        redirect_to signin_path if !signed_in?
+    end
 end
