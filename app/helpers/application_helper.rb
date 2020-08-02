6bf67ba515503a63b3a5e29ff2c37f9f2487d7a8
Adamlatorraca <adam.latorraca@gmail.com>
2020-08-02 18:52:23 -0400

    add authorization methods

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,4 +13,13 @@ module ApplicationHelper
         flash[:error] = "Please Sign in."
         redirect_to signin_path if !signed_in?
     end
+
+    def authorized?
+        @hilite.user == current_user
+    end
+
+    def redirect_if_not_authorized
+        flash[:error] = "You can only edit your own HiLites."
+        redirect_to hilites_path if !authorized?
+    end
 end
