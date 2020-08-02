51b65398c5fb2583156d05f84e29a4d9c4f907c1
Adamlatorraca <adam.latorraca@gmail.com>
2020-08-02 15:40:29 -0400

    move helper methods to appropriate file

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,33 +1,3 @@
 class ApplicationController < ActionController::Base
-    helper_method :current_user, :signed_in?, :authorized?
-
-
-private
-  def current_user
-    # if limits db queries
-    @current_user ||= User.find_by_id(session[:user_id]) if session[:user_id]
-  end
-
-  def signed_in?
-    # most effiecient
-    !!session[:user_id]
-  end
-
-  def redirect_if_not_signed_in
-    flash[:error] = "Please Sign in."
-    redirect_to signin_path if !signed_in?
-  end
-
-  def authorized?
-    @hilite = Hilite.find(params[:id])
-    @hilite.user == current_user
-  end
-
-  def redirect_if_not_authorized
-    if !authorized?
-      flash[:error] = "You do not have the correct permissions to do that."
-      redirect_to hilites_path
-    end
-  end
-
+  include ApplicationHelper
 end
